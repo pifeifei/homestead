c463a83df7cce02d8e1deab7af49233d0f15814a
kis龍 <270228163@qq.com>
2021-12-03 09:49:37 +0800

    Set up domestic(chinese) mirror source

--- resources/after.sh
+++ resources/after.sh
@@ -21,3 +21,20 @@
 # More info: https://github.com/nodesource/distributions/blob/master/README.md#debinstall
 #curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 #sudo apt-get install -y nodejs
+
+# composer 阿里云镜像源
+composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+
+# php 扩展安装，时间可能会很长，国内慢
+#sudo pecl channel-update pecl.php.net
+#sudo pecl install swoole
+#sudo pecl install inotify
+
+# 阿里云镜像源
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/" /etc/apt/sources.list
+sudo sed -i "s/security.ubuntu.com/mirrors.aliyun.com/" /etc/apt/sources.list
+sudo apt-get clean
+
+# nodejs 国内镜像源，使用 cnpm install xxx，原 npm install xxx 不受影响
+sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
